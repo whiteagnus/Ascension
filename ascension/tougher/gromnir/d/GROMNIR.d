ddfc35fc9d9605f31f62d03e998dc3e203e9eabb
Matty B <MZBigelow@gmail.com>
2015-08-22 16:44:48 -0700

    See Description
    * Moved yaga1.spl from Ascension spl folder into tougher yagashura
    folder (Tougher Yagashura uses it, and I can't find references to
    yaga1.spl in the main Ascension component.
    * Broke down all GP scripts to the smallest possible IF-THEN blocks for
    maximum patching compatibility, using R_B_B. This'll also help track
    down bugs and incompatabilities when the Siege of Dragonspear patches
    are released for BG2:EE
    * Moved all GPMAGE, GPARCHER, GPMERC, GPTHIEF, and GPHEALER patching
    into a new folder, toughercommon, since these scripts are used by more
    than one component.
    * Updated .tp2 to reflect this, and added commentary (spotty though,
    will finish later after I take a break) so that anyone can hopefully
    know what the .tp2 is doing
    * I found what looked like to be a bug in the Ascension scripts
    involving the Randomwalk() command, and fixed it. If it turns out this
    was not a bug and intentional, it can be easily fixed by editing the
    TxCOMMON1.baf file in the toughercommon folder.
    * .tp2 now edits yaga-shura's area rather than overwrites
    * Changed Tougher Illasera to no longer apply drow disintegration code,
    as EE has made this redundant. Implemented two ascension changes to the
    relevant area script. Also change the EXTEND_BOTTOM script name to
    reflect the convention established by tougher Gromnir (no practical
    effect, just for consistency).

--- ascension/tougher/gromnir/d/GROMNIR.d
+++ ascension/tougher/gromnir/d/GROMNIR.d
@@ -1,1 +1,4 @@
+//JediMindTrix Additions, this replaces code that becomes unnecessary with WhiteAgnus' addition
+REPLACE_TRANS_ACTION GROMNIR BEGIN 7 END BEGIN END ~ActionOverride("gromg03",Shout(151)) ActionOverride("gromg03",Enemy()) Shout(151)~ ~~
+//WhiteAgnus Stuff
 ADD_TRANS_ACTION GROMNIR BEGIN 7 END BEGIN END ~SetGlobalTimer("reinfg","GLOBAL",10) ActionOverride("gromg03",Shout(ALERT)) ActionOverride("gromg05",Enemy()) ActionOverride("gromg08",Enemy()) ActionOverride("gromg09",Enemy()) ActionOverride("gromg10",Enemy())~
--- ascension/tougher/yagashura/d/yaga01.d
+++ ascension/tougher/yagashura/d/yaga01.d
@@ -1,1 +1,1 @@
-REPLACE_TRANS_ACTION YAGA01 BEGIN 5 END BEGIN 0 END ~SetGlobalTimer("YagaReturnTimer","AR5203",15)~ ~SetGlobalTimer("YagaReturnTimer","AR5203",100)~
+REPLACE_TRANS_ACTION BEGIN 5 END BEGIN END ~SetGlobalTimer("YagaReturnTimer","AR5203",15)~ ~SetGlobalTimer("YagaReturnTimer","AR5203",100)~
