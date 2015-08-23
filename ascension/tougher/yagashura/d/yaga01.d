da94fde55dd31f7f891ec4da990e1bab9904d4e4
whiteagnus <whiteagnus@gmx.de>
2015-08-23 18:00:18 +0200

    see description
    - moved ds_v3 to the main component
    - fixed several installation bugs
    - moved shared code to tpa

--- ascension/tougher/yagashura/d/yaga01.d
+++ ascension/tougher/yagashura/d/yaga01.d
@@ -1,1 +1,1 @@
-REPLACE_TRANS_ACTION BEGIN 5 END BEGIN END ~SetGlobalTimer("YagaReturnTimer","AR5203",15)~ ~SetGlobalTimer("YagaReturnTimer","AR5203",100)~
+REPLACE_TRANS_ACTION yaga01 BEGIN 5 END BEGIN END ~SetGlobalTimer("YagaReturnTimer","AR5203",15)~ ~SetGlobalTimer("YagaReturnTimer","AR5203",100)~
