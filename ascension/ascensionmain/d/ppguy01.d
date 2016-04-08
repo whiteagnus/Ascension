8900d4da1dba877bc9226bfe4716b3fcadfaa751
whiteagnus <whiteagnus@gmx.de>
2016-04-09 00:52:03 +0200

    Changed StateCheck "15" and "4032" back to Dead() Triggers...
    Played through 2.0 ToB today and I wasn't able to go to the Throne first
    (ppdugy01.d), then I defeated all 5 Bhaalspawns and Mellisan didn't get
    back to fight...
    Changed the triggers back to Dead("blubb") instead of
    StateCheck("blubb",15/4032) and everything worked fine for me... ;)

--- ascension/ascensionmain/d/ppguy01.d
+++ ascension/ascensionmain/d/ppguy01.d
@@ -1,3 +1,3 @@
 ADD_STATE_TRIGGER PPGUY01 0 ~Global("BalthazarFights","GLOBAL",0)~ 
 
-REPLACE_STATE_TRIGGER PPGUY01 4 ~OR(2) StateCheck("balth",4032) Global("BalthazarFights","GLOBAL",1)~ 
+REPLACE_STATE_TRIGGER PPGUY01 4 ~OR(2) Dead("balth") Global("BalthazarFights","GLOBAL",1)~ 
