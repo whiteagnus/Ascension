925fadda4b4e4b345e8ecd02a411d51e448658ad
Quitch <quitch@users.noreply.github.com>
2016-09-23 17:24:51 +0100

    Give Bodhi dialogue after Irenicus dies
    This hasn't been tested as I don't have a ToB save

--- ascension/ascensionmain/d/finbodh.d
+++ ascension/ascensionmain/d/finbodh.d
@@ -1,25 +1,25 @@
 BEGIN FINBODH
 
 IF ~Global("BodhiTalks","LOCALS",0) !GlobalGT("BodhiHappyBunny","LOCALS",0)~ THEN a0
-  SAY @33 
+  SAY @33
   IF ~~ THEN DO ~SetGlobal("BodhiTalks","LOCALS",1)~ EXTERN IRENIC2 a9
 END
 
 IF ~False()~ THEN a1
-  SAY @34 
+  SAY @34
   IF ~~ THEN EXTERN IRENIC2 a11
 END
 
 IF ~False()~ THEN a2
-  SAY @35 
+  SAY @35
   ++ @36  + a4
   ++ @37  + a30
   ++ @38  + a30
   ++ @39  + a31
 END
 
-IF ~~ THEN a4 
-  SAY @41 
+IF ~~ THEN a4
+  SAY @41
   ++ @42  + a21
   ++ @43  + a21
   + ~CheckStatGT(Player1,15,CHR) Global("BalthazarFights","GLOBAL",0) !Alignment(Player1,MASK_GOOD) !ReputationGT(Player1,11)~ + @44 + a22
@@ -33,40 +33,40 @@ IF ~False()~ THEN a19
   IF ~~ THEN EXTERN IRENIC2 a13
 END
 
-IF ~False()~ THEN a20 
+IF ~False()~ THEN a20
   SAY @47
   IF ~~ THEN EXTERN IRENIC2 a14
 END
 
-IF ~~ THEN a21 
+IF ~~ THEN a21
   SAY @48
   IF ~~ THEN EXTERN IRENIC2 a10
 END
 
-IF ~~ THEN a22 
+IF ~~ THEN a22
   SAY @49
   IF ~InPartyAllowDead("Sarevok") !StateCheck("Sarevok",4032)~ THEN GOTO a23
   IF ~OR(2) !InPartyAllowDead("Sarevok") StateCheck("Sarevok",4032)~ THEN GOTO a25
 END
 
-IF ~~ THEN a23 
+IF ~~ THEN a23
   SAY @50
   IF ~False()~ THEN GOTO a24
   IF ~True()~ THEN EXTERN IRENIC2 a12
 END
 
-IF ~~ THEN a24 
+IF ~~ THEN a24
   SAY @51
   IF ~False()~ THEN EXIT
   IF ~~ THEN GOTO a44
 END
 
-IF ~~ THEN a25 
+IF ~~ THEN a25
   SAY @52
   IF ~~ THEN EXTERN IRENIC2 a12
 END
 
-IF ~~ THEN a26 
+IF ~~ THEN a26
   SAY @53
   ++ @54 + a27
   ++ @55 + a28
@@ -82,12 +82,12 @@ IF ~~ THEN a27
   IF ~Global("BalthazarFights","GLOBAL",1) Global("WorkingForBodhi","GLOBAL",0)~ THEN GOTO a46
 END
 
-IF ~~ THEN a28 
+IF ~~ THEN a28
   SAY @58
   IF ~~ THEN EXTERN IRENIC2 a10
 END
 
-IF ~~ THEN a29 
+IF ~~ THEN a29
   SAY @59
   ++ @54 + a27
   ++ @55 + a28
@@ -95,27 +95,27 @@ IF ~~ THEN a29
   ++ @60 + a28
 END
 
-IF ~~ THEN a30 
+IF ~~ THEN a30
   SAY @61
   IF ~~ THEN EXTERN IRENIC2 a10
 END
 
-IF ~~ THEN a31 
+IF ~~ THEN a31
   SAY @62
   IF ~~ THEN EXTERN IRENIC2 a10
 END
 
-IF ~~ THEN a32 
+IF ~~ THEN a32
   SAY @63
   IF ~~ THEN EXTERN IRENIC2 a13
 END
 
-IF ~~ THEN a33 
+IF ~~ THEN a33
   SAY @64
   IF ~~ THEN EXTERN IRENIC2 a13
 END
 
-IF ~~ THEN a34 
+IF ~~ THEN a34
   SAY @65
   ++ @66  DO ~SetGlobal("BodhiPromised","GLOBAL",1)~ + a35
   ++ @67  DO ~SetGlobal("BodhiPromised","GLOBAL",1)~ + a35
@@ -123,22 +123,22 @@ IF ~~ THEN a34
   + ~CheckStatGT(Player1,15,WIS)~ + @69 + a20
 END
 
-IF ~~ THEN a35 
+IF ~~ THEN a35
   SAY @70
   IF ~~ THEN GOTO a20
 END
 
-IF ~~ THEN a36 
+IF ~~ THEN a36
   SAY @71
   IF ~~ THEN EXTERN IRENIC2 a13
 END
 
-IF ~False()~ THEN a37 
+IF ~False()~ THEN a37
   SAY @72
   IF ~~ THEN GOTO a38
 END
 
-IF ~~ THEN a38 
+IF ~~ THEN a38
   SAY @73
   ++ @74  DO ~SetGlobal("BodhiHappyBunny","LOCALS",1) ClearAllActions() StartCutSceneMode() StartCutScene("bodhif01")~ EXIT
   ++ @75  + a39
@@ -155,7 +155,7 @@ IF ~~ THEN a39
   + ~Global("BodhiPromised","GLOBAL",0)~ + @82  DO ~SetGlobal("BodhiFights","GLOBAL",0) ClearAllActions() StartCutSceneMode() StartCutScene("bodhif02")~ EXIT
 END
 
-IF ~~ THEN a40 
+IF ~~ THEN a40
   SAY @83
   IF ~Global("BodhiPromised","GLOBAL",1)~ THEN DO ~SetGlobal("BodhiHappyBunny","LOCALS",3)~ EXTERN FINSOL01 a40
   + ~Global("BodhiPromised","GLOBAL",0)~ + @79  DO ~SetGlobal("BodhiFights","GLOBAL",0) ClearAllActions() StartCutSceneMode() StartCutScene("bodhif02")~ EXIT
@@ -179,7 +179,7 @@ IF ~Global("BodhiHappyBunny","LOCALS",3)~ THEN a43
   IF ~~ THEN DO ~SetGlobal("BodhiFights","GLOBAL",2) ClearAllActions() StartCutSceneMode() StartCutScene("bodhif03")~ EXIT
 END
 
-IF ~~ THEN a44 
+IF ~~ THEN a44
   SAY @87
   ++ @66 DO ~SetGlobal("BodhiPromised","GLOBAL",1)~ + a45
   ++ @67 DO ~SetGlobal("BodhiPromised","GLOBAL",1)~ + a45
@@ -195,4 +195,19 @@ END
 IF ~~ THEN a46
   SAY @90
   IF ~~ THEN EXTERN IRENIC2 a13
+END
+
+IF ~Global("FinalFight","AR6200",4)~ THEN a47
+  SAY @91
+  IF ~~ THEN GOTO a48
+END
+
+IF ~~ THEN a48
+  SAY @92
+  IF ~~ THEN GOTO a49
+END
+
+IF ~~ THEN a49
+  SAY @93
+  IF ~~ THEN DO ~SetGlobal("FinalFight","AR6200",5)~ EXIT
 END
